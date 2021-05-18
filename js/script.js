7fdd2f7d1896e4b4ce868791f088ac184259eab5
Gleb <glebzhrnv13@mail.ru>
2021-05-18 18:28:21 +0300

    домашнее задание

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// task-1
+// const array = [1, 2, 3, 4, 5, 6, 7];
+// const removeElement = array.splice(5, 1)
+// console.log(array);
+
+// task-4
+// const array1 = [7, -2, 10, 5, 0];
+// const array2 = [0, 10];
+// let difference = array1.filter(x => !array2.includes(x));
+// console.log(difference)
+
